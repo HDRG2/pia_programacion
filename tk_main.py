4fc37725dbedcce80341eb1cfc13dc84764be5f6
SebasRG99 <pablosebastianruizgallegos9@gmail.com>
2024-04-25 00:25:51 -0600

    feat: se esta creando el login, la ventana inicial ya es el login

--- tk_main.py
+++ tk_main.py
@@ -1,5 +1,6 @@
 from tkinter import Tk
-from main import main_menu
+# from main import main_menu
+from auth.login import login
 
 root = Tk()
 
@@ -8,7 +9,8 @@ def main_tk():
     root.config(bg='Gray')
     root.geometry("300x500")
     root.resizable(width=0, height=0)
-    main_menu(root)
+    # main_menu(root)
+    login(root)
     root.mainloop()
     
 if __name__ == "__main__":
