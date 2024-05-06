0ffbea4c6edbc36179295c0cf4bed49f596c81c7
HDRG2 <hectordanielruizgallegos@gmail.com>
2024-05-06 13:51:57 -0600

    Se trabajo con las carpetas de Empleado y Proveedor, tambien se agrego una carpeta nueva de TipoProveedor

--- Proveedores/agregar.py
+++ Proveedores/agregar.py
@@ -8,9 +8,11 @@ def agregar_proveedores(root,proveedores_menu,main_menu):
     
     def destroy_elements():
         titulo.destroy()
+        idProveedor.destroy()
         nombre.destroy()
         button_regresar.destroy()
-        nombre_label.destroy()
+        nombre_label1.destroy()
+        nombre_label2.destroy()
             
            
     root.title("Agregando Proveedor")
@@ -18,10 +20,18 @@ def agregar_proveedores(root,proveedores_menu,main_menu):
     titulo = Label(root,text="PROVEEDOR",bg="gray")
     titulo.grid(row=0,column=0,sticky="N",padx=(30,0),pady=(20,0))
 
-    nombre_label = Label(root, text="RFCProveedor:", bg="gray")
-    nombre_label.grid(row=1, column=0, pady=(10,0))
+    nombre_label1 = Label(root, text="idProveedor:", bg="gray")
+    nombre_label1.grid(row=1, column=0, pady=(10,0))
+    idProveedor = Entry(root)
+    idProveedor.grid(row=1,column=1,pady=(10,0))
+    
+    nombre_label2 = Label(root,text="Nombre:",bg="gray")
+    nombre_label2.grid(row=2,column=0,pady=(10,0))
     nombre = Entry(root)
-    nombre.grid(row=1,column=1,pady=(10,0))
+    nombre.grid(row=2,column=1,pady=(10,0))
+    
+    
+    
     
     button_regresar = Button(root,text="<==",command=lambda: previous_page())
     button_regresar.grid(row=2,column=0,padx=0,pady=0)
--- Proveedores/eliminar.py
+++ Proveedores/eliminar.py
@@ -1,9 +1,9 @@
 from tkinter import *
 
-def eliminar_empleado(root,empleado_menu,main_menu):
+def eliminar_Proveedores(root,proveedores_menu,main_menu):
     def previous_page():
         destroy_elements()
-        empleado_menu(root,main_menu)
+        proveedores_menu(root,main_menu)
     
     def destroy_elements():
         titulo.destroy()
@@ -15,12 +15,12 @@ def eliminar_empleado(root,empleado_menu,main_menu):
     def boton_buscar():
         print("Se esta opriminedo el boton buscar")
         
-    root.title("Eliminando Empleado")
+    root.title("Eliminando Proveedor")
     
     titulo = Label(root,text="ELIMINAR",bg="gray")
     titulo.grid(row=0,column=0,sticky="N",padx=(30,0),pady=(20,0))
    
-    nombre_label = Label(root,text="idEmpleado:",bg="gray")
+    nombre_label = Label(root,text="idProveedor:",bg="gray")
     nombre_label.grid(row=1,column=0,pady=(10,0))
     nombre = Entry(root)
     nombre.grid(row=1,column=1,pady=(10,0))
